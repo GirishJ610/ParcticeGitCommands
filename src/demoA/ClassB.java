e2528a60f967353c566c80de6740fcde43ff168a
Girish J <giri.j.nagaragere@gmail.com>
2026-06-25 19:09:58 +0530

    commit from y

--- src/demoA/ClassB.java
+++ src/demoA/ClassB.java
@@ -5,6 +5,10 @@ public class ClassB {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("classB Test case 1");
+		System.out.println("classB Test case 2");
+		System.out.println("classB Test case 3");
+		System.out.println("classB Test case 4");
+		System.out.println("classB Test case 5");
 
 	}
 

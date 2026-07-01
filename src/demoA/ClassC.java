b2d0adc1612b871c0ecc5b115c8789205208be47
Girish J <giri.j.nagaragere@gmail.com>
2026-07-01 11:47:55 +0530

    new

--- src/demoA/ClassC.java
+++ src/demoA/ClassC.java
@@ -9,6 +9,8 @@ public class ClassC {
 		System.out.println("class C Test case 3");
 		System.out.println("class C Test case 4");
 		System.out.println("class C Test case 5	");
+		
+		System.out.println("new");
 
 	}
 
